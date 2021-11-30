bff48407a9b5e46b7c85ca647bbac0756869dc2d
Matt Burns <m6urns@gmail.com>
2021-11-29 17:06:27 -0800

    Cleaned up scrapper

--- clean/scrape_production.R
+++ clean/scrape_production.R
@@ -6,36 +6,6 @@ rD <- rsDriver(browser=c("firefox"))
 
 driver <- rD[["client"]]
 
-# Navigate to and select year
-driver$navigate("https://www.accessdata.fda.gov/scripts/cder/outsourcingfacility/index.cfm")
-
-driver$navigate("https://www.accessdata.fda.gov/scripts/cder/outsourcingfacility/index.cfm/#report")
-
-element <- driver$findElement(using = "css", "#report")
-driver$mouseMoveToLocation(webElement = element)
-driver$click()
-
-element$sendKeysToElement(list("2019-1"))
-
-driver$navigate("https://www.accessdata.fda.gov/scripts/cder/outsourcingfacility/index.cfm/#report")
-
-
-# Navigate to and select Type
-driver$navigate("https://www.accessdata.fda.gov/scripts/cder/outsourcingfacility/index.cfm")
-
-driver$navigate("https://www.accessdata.fda.gov/scripts/cder/outsourcingfacility/index.cfm/#sugg")
-
-element <- driver$findElement(using = "css", "#sugg")
-driver$mouseMoveToLocation(webElement = element)
-driver$click()
-
-#Weird, there must be a bug here in the FDA form, est selects active
-#active selects est
-element$sendKeysToElement(list("Establishment"))
-
-driver$navigate("https://www.accessdata.fda.gov/scripts/cder/outsourcingfacility/index.cfm/#sugg")
-
-
 driver$navigate("https://www.accessdata.fda.gov/scripts/cder/outsourcingfacility/index.cfm")
 
 option <- driver$findElement(using = 'xpath', "//*[(@id = 'report')]/option[@value = '2020-1']")
@@ -55,78 +25,13 @@ element <- driver$findElement(using = "css", ".btn-primary")
 driver$mouseMoveToLocation(webElement = element)
 driver$click()
 
-###
-element <- driver$findElement(using = "css", "#report > option:nth-child(1)")
-driver$mouseMoveToLocation(webElement = element)
-driver$click()
-
-element <- driver$findElement(using = "xpath", "//*[@id='report']")
-driver$mouseMoveToLocation(webElement = element)
-driver$click()
-
-option <- driver$findElement(using "xpath", "")
-
-option <- driver$findElement(using = "xpath", "//*/option[@value = '2020-1']")
-
-driver$mouseMoveToLocation(webElement = element)
-driver$click()
-
-element <- driver$findElement(using = "xpath", "//*[@id='report']/option[@value='2019-1']")
-driver$mouseMoveToLocation(webElement = element)
-driver$click()
-
-option <- driver$findElement(using = "xpath", "//*/option[@value='2019-1']")
-driver$mouseMoveToLocation(webElement = option)
-driver$click()
-
-option <- driver$findElement(using = "xpath", "//*[@id='report']/option[2]")
-driver$mouseMoveToLocation(webElement = option)
-driver$click()
-
-element <- driver$findElement(using = "xpath", "//*[@id='report']/option[@value='2019-1']")
-driver$mouseMoveToLocation(webElement = element)
-driver$click()
-
-element <- driver$findElement(using = "xpath", "//*[@id='report']")
-driver$mouseMoveToLocation(webElement = element)
-driver$click()
-
-element$sendKeysToElement(list("2020-1"))
-element$sendKeysToElement(list(key="enter"))
-driver$click()
-
-
-###
-
-element <- driver$findElement(using = "css", "#sugg")
-driver$mouseMoveToLocation(webElement = element)
-driver$click()
-
-element$sendKeysToElement(list("Active"))
-driver$click()
-
-element <- driver$findElement(using = "css", "#searchbox")
-driver$mouseMoveToLocation(webElement = element)
-driver$click()
-
-element$sendKeysToElement(list("Empower Pharmacy"))
-
-#Search
-element <- driver$findElement(using = "css", ".btn-primary")
-driver$mouseMoveToLocation(webElement = element)
-driver$click()
-
 #Download CSV
 element <- driver$findElement(using = "css", ".buttons-html5")
 driver$mouseMoveToLocation(webElement = element)
 driver$click()
-
-element <- driver
-#driver$navigate("https://www.accessdata.fda.gov/scripts/cder/outsourcingfacility/index.cfm")
 
 #close the driver
 driver$close()
 
 #close the server
 rD[["server"]]$stop()
-
