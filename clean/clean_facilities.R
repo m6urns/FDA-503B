f732673c2a881889575c007889239a02e5b43658
Matt Burns <m6urns@gmail.com>
2021-11-30 22:54:36 -0800

    Sep names and phone #'s

--- clean/clean_facilities.R
+++ clean/clean_facilities.R
@@ -3,10 +3,31 @@ require(tidyverse)
 require(readr)
 
 #Import scraped data
-facilities <- read_csv("FDA-503B/scrape/data/facilitytable_112921.csv", 
-                                 col_types = cols(`Initial Date of Registration as an Outsourcing Facility1` = col_date(format = "%m/%d/%Y"), 
-                                                  `Date of Most Recent Registration as an Outsourcing Facility1` = col_date(format = "%m/%d/%Y"), 
-                                                  `End Date of Last FDA Inspection Related to Compounding2` = col_date(format = "%m/%d/%Y")))
+facility_table_raw <- read_csv("FDA-503B/scrape/data/FacilityTable_11_3_2021.csv", 
+                           col_types = cols(`Initial Date of Registration as an Outsourcing Facility1` = col_date(format = "%m/%d/%Y"), 
+                                                     `Date of Most Recent Registration as an Outsourcing Facility1` = col_date(format = "%m/%d/%Y"), 
+                                                     `End Date of Last FDA Inspection Related to Compounding2` = col_date(format = "%m/%d/%Y")))
+
+#Drop X1 & Correct Column Names
+facility_table_reform <- facility_table_raw %>%
+  select(-X1) %>%
+  rename(`Initial Date of Registration as an Outsourcing Facility` = `Initial Date of Registration as an Outsourcing Facility1`,
+         `Date of Most Recent Registration as an Outsourcing Facility` = `Date of Most Recent Registration as an Outsourcing Facility1`,
+         `End Date of Last FDA Inspection Releated to Compounding` = `End Date of Last FDA Inspection Related to Compounding2`,
+         `Was a Form FDA-483 issued?` = `Was a Form FDA-483 issued?3`,
+         `Other Action, if Any, Based on Last Inspection` = `Other Action, if Any, Based on Last Inspection4,5`,
+         `Intends to Compounds Sterile Drugs From Bulk Drug Substances` = `Intends to Compounds Sterile Drugs From Bulk Drug Substances6`)
 
 #Seperate Contact Name and Phone Number
+
+names_numbers <- facility_table_reform$`Contact Name and Phone Number` %>%
+  str_split_fixed(pattern = "(?<=[a-zA-Z\\s])(?=[0-9])", n = 2)
+#names_numbers[ ,2]
+
+facility_table_reform_1 <- facility_table_reform %>%
+  select(-`Contact Name and Phone Number`) %>%
+  bind_cols(names_numbers[ ,1]) %>%
+  bind_cols(names_numbers[ ,2]) %>%
+  rename(`Contact Name` = ...8, `Phone Number` = ...9)
+  
 
--- clean/clean_production.R
+++ clean/clean_production.R
@@ -1,2 +1,36 @@
 require(tidyverse)
+require(readr)
 
+production_data_raw <- read_csv("FDA-503B/scrape/data/Production_2019+2020_11_30_2021.csv")
+
+# raw_product_list_for <- raw_product_list %>%
+#   select(-X1, -`Active Ingredients Info`, -`Package Description`, -`NDC Package Code`) %>%
+#   separate(`Estab. Name`, into = c("Facility", "UID"), sep = "\\(") %>%
+#   separate(`Report Year`, into = c("Year", "Period"), sep = "\\-") %>%
+#   rename(Active = `Active Ingredients`)
+# 
+# raw_product_list_for$UID <- gsub("\\)", "", as.character(raw_product_list_for$UID))
+
+## Drop X1 and Separate Facility Name from Facility UID
+production_data_reform <- production_data_raw %>%
+  select(-X1) %>%
+  separate(`Estab. Name`, into = c("Facility", "Facility UID"), sep = "\\(")
+
+# Drop the final ) from the end of facility UID
+production_data_reform$`Facility UID` <- gsub("\\)", "", as.character(production_data_reform$`Facility UID`))
+
+# Remove duplicate entries just in case. Scrapper uses several possible names for
+# some facilities because of buy outs, etc. Removing duplicates is ok because 
+# production reports do not reflect unique production runs ness, but reflect
+# production of an active in unique conc and delivery method. 
+
+# production_data_reform for 2019+2020 has 10,207 entries
+
+production_data <- production_data_reform %>%
+  distinct(across(everything()))
+
+# production data for 2019+2020 has 9,791 entries. Scrapping at some point is 
+# downloading the same data twice. 
+
+# Save production data to new csv in clean data directory
+write.csv(production_data, "~/FDA-503B/clean/data/Production_2019+2020_11_30_2021.csv")
