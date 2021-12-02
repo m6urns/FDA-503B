0cddb8876ff12e67dccfa104413de3687d5cc25e
Matt Burns <m6urns@gmail.com>
2021-12-02 09:44:05 -0800

    Essential celaning done for now, moving to databasing

--- clean/clean_facilities.R
+++ clean/clean_facilities.R
@@ -2,13 +2,17 @@
 require(tidyverse)
 require(readr)
 
-#Import scraped data
+# Import scraped data
 facility_table_raw <- read_csv("FDA-503B/scrape/data/FacilityTable_11_3_2021.csv", 
                            col_types = cols(`Initial Date of Registration as an Outsourcing Facility1` = col_date(format = "%m/%d/%Y"), 
                                                      `Date of Most Recent Registration as an Outsourcing Facility1` = col_date(format = "%m/%d/%Y"), 
                                                      `End Date of Last FDA Inspection Related to Compounding2` = col_date(format = "%m/%d/%Y")))
 
-#Drop X1 & Correct Column Names
+#Import uscities for location matching
+uscities <- read_csv("~/FDA-503B/databases/uscities.csv") %>%
+  select(city)
+
+# Drop X1 & Correct Column Names
 facility_table_reform <- facility_table_raw %>%
   select(-X1) %>%
   rename(`Initial Date of Registration as an Outsourcing Facility` = `Initial Date of Registration as an Outsourcing Facility1`,
@@ -18,16 +22,50 @@ facility_table_reform <- facility_table_raw %>%
          `Other Action, if Any, Based on Last Inspection` = `Other Action, if Any, Based on Last Inspection4,5`,
          `Intends to Compounds Sterile Drugs From Bulk Drug Substances` = `Intends to Compounds Sterile Drugs From Bulk Drug Substances6`)
 
-#Seperate Contact Name and Phone Number
-
+# Separate Contact Name and Phone Number
 names_numbers <- facility_table_reform$`Contact Name and Phone Number` %>%
   str_split_fixed(pattern = "(?<=[a-zA-Z\\s])(?=[0-9])", n = 2)
-#names_numbers[ ,2]
+
 
 facility_table_reform_1 <- facility_table_reform %>%
   select(-`Contact Name and Phone Number`) %>%
   bind_cols(names_numbers[ ,1]) %>%
   bind_cols(names_numbers[ ,2]) %>%
-  rename(`Contact Name` = ...8, `Phone Number` = ...9)
+  rename(`Contact Name` = ...8, `Phone Number` = ...9)S
   
+# Separate State and City
+# state <- facility_table_reform_1$`Facility Name` %>%
+#   str_split_fixed(pattern = "(?=[^ ]+$)", n = 2)
+# 
+# facility_table_reform_2 <- facility_table_reform_1 %>%
+#   select(-`Facility Name`) %>%
+#   bind_cols(state[ ,1]) %>%
+#   bind_cols(state[ ,2]) %>%
+#   rename(`Facility Name` = ...9, State = ...10)
 
+# facility_table_reform_2$`Facility Name` <- gsub("\\,", "", as.character(facility_table_reform_2$`Facility Name`))
+
+# Separate Facility Name and City
+
+# facility_table_reform_2 %>%
+#   add_column(new_col = "City")
+# 
+# for (i in seq_along(facility_table_reform_2)) {
+#   if (facility_table_reform_2$`Facility Name`[[i]] %in% uscities) {
+#     city <- filter(uscities == facility_table_reform_2$`Facility Name`[[i]])
+#     facility_table_reform_2$City[[i]] = city
+#   }
+#   else {
+#     facility_table_reform_2$City[[i]] = "NA"
+#   }
+# }
+# 
+# for (i in seq_along(facility_table_reform_2)) {
+#   if (grepl(facility_table_reform_2$`Facility Name`[[i]], uscities)) {
+#     city <- filter(uscities = facility_table_reform_2$`Facility Name`[[i]])
+#     facility_table_reform_2$City[[i]] = city
+#   }
+#   else {
+#     facility_table_reform_2$City[[i]] = "NA"
+#   }
+# }
